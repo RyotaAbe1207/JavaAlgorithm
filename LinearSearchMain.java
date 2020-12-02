2ba4e223608cd909daee5273e501ebc9c96e1dfb
Ryota Abe <ryotaabe@RyotanoMacBook-Pro.local>
2020-12-02 12:36:50 +0900

    first commit

---
+++ LinearSearchMain.java
@@ -0,0 +1,21 @@
+package chapter2;
+
+public class LinearSearchMain {
+	public static void main(String[] args) {
+		LinearSearch table = new LinearSearch();
+
+		table.add(1,"one");
+		table.add(10,"ten");
+		table.add(2,"two");
+
+		String x;
+		x = (String)table.search(10);
+		if(x != null) {
+			System.out.println("value=" + x);
+		}else {
+			System.out.println("Not fonud");
+		}
+
+	}
+
+}
